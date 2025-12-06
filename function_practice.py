1f927a5d2f1356787a9770b053fd013886d04d33
Tim-Irwin <tmthyirwin@gmail.com>
2025-12-05 21:17:05 -0600

    More fuction practice and notes.

--- function_practice.py
+++ function_practice.py
@@ -48,7 +48,7 @@ def get_formatted_name(first_name, last_name):
 myself = get_formatted_name('Timothy', 'Irwin')
 print(myself)
 
-#return a dictionary
+#retur:n a dictionary
 def build_person(first_name, last_name, age=None):
     '''return a dictionary regarding a person'''
     person = {'first': first_name, 'last': last_name}
@@ -58,3 +58,49 @@ def build_person(first_name, last_name, age=None):
 
 myself = build_person('Timothy', 'Irwin', age=30)
 print(myself)
+
+#passing a list as a parameter
+user_list = ['Timothy', 'Carl', 'Jane']
+def user_greeting(users):
+    '''print a greeting for each user'''
+    for user in users:
+        print(f"Hello, {user.title()}!")
+user_greeting(user_list)
+
+# pass a copy of a list so you dont modify the original
+# function_name(list_name[:])
+
+#passing an unknown number of arguments
+#you can mix unkown number of arguments and positional arguments.
+def make_cake(flavor, *ingredients):
+    '''print a list of ingredients'''
+    print(f'Making a {flavor} cake with the following ingredients:')
+    for ingredient in ingredients:
+        print(ingredient)
+        
+make_cake('chocolate', 'frosting', 'sprinkles', 'candle')
+
+#using an abritray number of keywordd arguments
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user."""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+user_profile = build_profile('albert', 'einstein',
+                             location='princeton',
+                             field='physics')
+print(user_profile)
+
+#importing an entire module use format
+#module_name.function_name()
+
+#importing a specific function from a module use format
+#from module_name import function_name
+
+#using as to give a funtion an alias use format
+#from module_name import function_name as fn
+
+#importing all functions from a module
+#from module_name import *
+
