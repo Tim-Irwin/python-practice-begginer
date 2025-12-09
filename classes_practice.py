93df345bbeb289bc4720359200f4b84afee9ef15
Tim-Irwin <tmthyirwin@gmail.com>
2025-12-09 17:23:09 -0600

    added demonstration of superclass  and subclass. Overiding methods in the superclass

--- classes_practice.py
+++ classes_practice.py
@@ -40,3 +40,57 @@ class Restaurant:
 restaurant_one = Restaurant('Georgios', 'pizza')
 restaurant_one.describe_restaurant()
 restaurant_one.open_restaurant()
+
+print('\n')
+
+class Car:
+    '''A simple class to represent a car'''
+
+    def __init__(self, make, model, year):
+        '''Initialize attributes'''
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer = 590
+
+    def get_descriptive_name(self):
+        '''return a formated car description'''
+        description = f"{self.year}, {self.make} {self.model}"
+        return description.title()
+    
+    def read_odometer(self):
+        print(f"The car has {self.odometer} miles on it.")
+
+    def update_odometer(self, miles):
+        if miles >= self.odometer:
+            self.odometer = miles
+        else:
+            print('You are not allowed to roll back an odometer!')
+    
+my_new_car = Car('Audi', "R8", 2026)
+print(my_new_car.get_descriptive_name())
+my_new_car.read_odometer()
+my_new_car.update_odometer(560)
+my_new_car.read_odometer()
+
+#A child class
+class ElectricCar(Car):
+    '''Has all aspects of the parent class Car.'''
+
+    def __init__(self, make, model, year):
+        '''Initialize attributes'''
+        super().__init__(make, model, year)
+        self.battery = 40
+    
+    def battery_size(self):
+        print(f"The battery size is {self.battery}-kWh.")
+
+    def get_descriptive_name(self):
+        '''Overide a superclass method'''
+        description = f"A {self.make} {self.model} from {self.year}."
+        return description
+
+my_electric_car = ElectricCar('Chevy', 'Bolt', 2018)
+print(my_electric_car.get_descriptive_name())
+my_electric_car.battery_size()
+
